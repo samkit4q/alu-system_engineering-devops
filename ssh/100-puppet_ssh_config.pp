fa269737ff95fefb7d3cf8f4660bcea7880596b3
samkit4q <a.aderoba@alustudent.com>
2024-03-16 10:40:29 -0700

    100-puppet_ssh_config.pp

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,4 @@
+# Puppet script to configure SSH client settings
 file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/sshd_config',
@@ -8,5 +9,14 @@ file_line { 'Declare identity file':
   ensure => present,
   path   => '/home/ubuntu/.ssh/config',
   line   => 'IdentityFile ~/.ssh/school',
+}
+
+file_line { 'Configure SSH client settings':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => [
+    'IdentityFile ~/.ssh/school',
+    'PasswordAuthentication no',
+  ],
 }
 
