b0e6c381d324c1f33d6b70cd9fa412450f10d3e8
samkit4q <a.aderoba@alustudent.com>
2024-03-17 06:30:30 -0700

    100-puppet_ssh_config.pp

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# using puppet to make changes to our configuration file
+
+file { 'ect/ssh/ssh_config':
+        ensure => present,
+
+content =>"
+
+        #SSH client configuration
+        host"
+        IdentiryFile ~/.ssh/school
+        PasswordAuthentication no
+
+}
