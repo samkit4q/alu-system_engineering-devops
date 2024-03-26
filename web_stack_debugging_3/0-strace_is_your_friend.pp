26799131a8c65a65b87c3077168cde880236ae38
samkit4q <a.aderoba@alustudent.com>
2024-03-26 07:00:37 -0700

    0-strace_is_your_friend.pp

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -13,3 +13,16 @@ file { '/etc/apache2/apache2.conf':
   notify  => Exec['restart-apache'],
 }
 
+# Define an exec resource to check Apache status
+exec { 'check-apache-status':
+  command => '/usr/sbin/service apache2 status',
+  unless  => '/usr/sbin/service apache2 status | grep -q "Apache2 is running"',
+}
+
+# Define an exec resource to check if Apache is serving the correct page
+exec { 'check-apache-page':
+  command => '/usr/bin/curl -s http://localhost/ | grep -q "Holberton"',
+  require => Exec['restart-apache'],
+}
+
+
